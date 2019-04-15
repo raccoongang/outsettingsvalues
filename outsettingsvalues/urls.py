736055d2fce6a7991e5ee39ff2b8347a252b1ae6
Golub Sergey <1.golub.sergey.1@gmail.com>
2019-04-15 11:48:41 +0300

    added search form, custom tags for checking type of values of settings vars

--- outsettingsvalues/urls.py
+++ outsettingsvalues/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.BaseView.as_view())
+    url(r'^$', views.BaseView.as_view(), name='base')
 ]
--- outsettingsvalues/views.py
+++ outsettingsvalues/views.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.views.generic import TemplateView
-import django.conf.global_settings as global_settings_current
 from django.conf import settings
+from django.views.generic import TemplateView
+
+from .forms import SettingForm
+
+import types
 
 class BaseView(TemplateView):
     template_name = 'outsettingsvalues/index.html'
 
+
     def get_context_data(self, **kwargs):
         context = super(BaseView, self).get_context_data(**kwargs)
-        # temp = {}
-        # for i in dir(global_settings_current):
-        #     temp[i] = dir(i)
-        # context['settings_LMS'] = settings.LMS_BASE
-        # context['settings_CMS'] = settings.CMS_BASE
-        # context['settings_list'] = temp
-
+        settings_list = {}
+        for i in dir(settings):
+            if callable(i) or isinstance(i, types.FunctionType) or i.startswith('__'):
+                continue
+            settings_list[i] = getattr(settings, i)
+        context['form'] = SettingForm(data=self.request.GET or None)
+        if context['form'].is_valid():
+            temp_list = {}
+            for key in settings_list.keys():
+                if context['form'].cleaned_data['setting_name'] in key:
+                    temp_list[key] = settings_list[key]
+            context['settings_list'] = temp_list
+        else:
+            context['settings_list'] = settings_list
         return context
